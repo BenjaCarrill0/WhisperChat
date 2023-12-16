ebdbf0bc3769d5d667997fe8e617954da9140d09
Benjamin Carrillo <carrillohermosillabenjamin@gmail.com>
2023-12-16 10:54:57 -0300

    ..

--- server/server.py
+++ server/server.py
@@ -1,49 +1,55 @@
 import socket
 import threading
-# host = "localhost"
-# port = 1800
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# sock.bind((host, port))
-# sock.listen()
-# print("El server está escuchando")
 
-# try:
-#     sock_client, (ip_client, port_client) = sock.accept()
-#     print(f"Se acaba de aceptar a la ip {ip_client} con port {port_client}")
-#     while True:
-#         print("Ahora se recibirá el largo del mensaje")
-#         lenght_msg = int.from_bytes(sock_client.recv(4), byteorder = "big")
-#         if lenght_msg == 0:
-#             break
-#         print(f"El largo es de {lenght_msg} bytes")
-#         msg = sock_client.recv(lenght_msg).decode("utf-8")
-#         print(msg)
-# except ConnectionError:
-#     print("Hubo algún error en la conexión con el cliente")
-
-# finally:
-#     sock.close()
-#     print("Aquí termina todo")
+CHUNK_SIZE = 32
 
 class Server:
     def __init__(self, host, port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((host, port))
-        self.online_users = set()
+        self.lock_users = threading.Lock()
+        self.lock_connections = threading.Lock()
+        self.online_users = dict()
         self.sock.listen()
         self.accept_connections()
     
     def accept_connections(self):
-        #This function 
+        #This function creates a thread that all the time
+        #Recieves differents connections
         thread = threading.Thread(target = self.accept_connections_thread)
         thread.start()
 
     def accept_connections_thread(self):
         while True:
-            client_sock, _ = self.sock.accept()
+            client_sock, (ip_client, port_client) = self.sock.accept()
             listening_client_thread = threading.Thread(target = self.listen_client,
                                                        args = (client_sock, ),
                                                        daemon = True)
             listening_client_thread.start()
     def listen_client(self, client_sock):
-        lenght_msg = int.from_bytes(self.sock.recv(4), byteorder = "big")
+        lenght_user = int.from_bytes(self.sock.recv(4), byteorder = "big")
+        user = self.sock.recv(lenght_user).decode("utf-8")
+        #Now we add the user to the dict of online_users
+        #That's because when we want to send a message to another user
+        #Then we only use the username and the server use the socket to send
+        #the message
+        with self.lock_users:
+            self.online_users[user] = client_sock
+        #Now we can listen all messages of the user
+        while True:
+            with self.lock_connections:
+                lenght_msg = self.sock.recv(4)
+                if lenght_msg == 0:
+                    break
+                lenght_target = self.sock.recv(4)
+                if lenght_target == 0:
+                    continue
+                    #With this in case the user doesn't
+                    #put any user. Then try another time
+                target_user = self.recv(lenght_target).decode("utf-8")
+                if target_user not in self.online_users:
+                    error_msg = f"{target_user} is not online at this moment."
+                    error_byte = error_msg.encode("utf-8")
+                    lenght = len(error_byte).to_bytes(length = 4, byteorder = "big")
+                    
+                    
