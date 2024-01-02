43db78be49edc1ab0209d9248b0d67085cb2b2d3
Benjamin Carrillo <carrillohermosillabenjamin@gmail.com>
2024-01-02 18:57:52 -0300

    At this point I forget what i was doing

--- client/user.py
+++ client/user.py
@@ -1,22 +1,56 @@
 import socket
+import threading
 
-# host = "localhost"
-# port = 1800
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+CHUNK_SIZE = 32
 
-# try:
-#     sock.connect((host, port))
-#     print("Conexión realizada correctamente")
-#     while True:
-#         msg = input("Introduce el mensaje que quieres enviar:\n")
-#         msg_bytes = msg.encode("utf-8")
-#         lenght_msg = len(msg_bytes).to_bytes(length = 4, byteorder = "big")
-#         sock.sendall(lenght_msg + msg_bytes)
+class User:
+    def __init__(self, host_server, port_server):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.sock.connect((host_server, port_server))
+            self.recieve_user()
+            self.listen()
+            self.send()
+        finally:
+            self.sock.close()
 
-# except ConnectionError as e:
-#     print(f"Error: {e}")
-#     print("Se genero un error, posiblemente porque el server no está prendido")
+    def recieve_user(self):
+        print("Tell me your username:", end = " ")
+        username = input()
+        print("Now your password:", end = " ")
+        password = input()
+        self.send_info(username, password)
 
-# finally:
-#     sock.close()
-#     print("Aquí termina todo")
+    def send_info(self, username, password):
+        user = username.encode("utf-8")
+        lenght_user = len(user).to_bytes(length = 4, byteorder = "big")
+        password = password.encode("utf-8")
+        lenght_password = len(password).to_bytes(length = 4, byteorder = "big")
+        full_msg = bytearray()
+        full_msg.extend(lenght_user + user + lenght_password + password)
+        sended = 0
+        while sended < len(full_msg):
+            sended += self.sock.send(full_msg[sended:])
+
+    def listen(self):
+        listening = threading.Thread(target = self.listen_thread, daemon = True)
+        listening.start()
+
+    def listen_thread(self):
+        while True:
+            lenght_msg = int.from_bytes(self.sock.recv(4), byteorder = "big")
+            msg = bytearray()
+            while len(msg) < lenght_msg:
+                lenght_recv = min(CHUNK_SIZE, lenght_msg - len(msg))
+                msg.extend(self.sock.recv(lenght_recv))
+            print(msg)
+
+    def send(self):
+        sending = threading.Thread(target = self.send_thread, daemon = True)
+        sending.start()
+
+    def send_thread(self):  
+        while True:
+            user = input("Tell me who you want to send a message: ", end = "")
+            msg = input("What do you want to say?", end = "\n")
+            
